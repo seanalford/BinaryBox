3a749818fda58fa867f9fb2317a79993797cb614
sean.alford <sean.alford@icloud.com>
2020-02-27 14:11:58 -0500

    WIP Added Fody, and Fody.PropertyChanged, removed several Connection settings from IConnectionSettings.

--- src/Toolbox/Connection/ConnectionSettings.cs
+++ src/Toolbox/Connection/ConnectionSettings.cs
@@ -1,27 +1,21 @@
-﻿using ReactiveUI.Fody.Helpers;
+﻿using System.ComponentModel;
 
 namespace Toolbox.Connection
 {
-    public class ConnectionSettings : IConnectionSettings
+    public class ConnectionSettings : IConnectionSettings, INotifyPropertyChanged
     {
-        public const int DEFAULT_CONNECT_RETIRES = 3000;
         public const int DEFAULT_RECEIVE_BUFFER_SIZE = 1024;
-        public const int DEFAULT_RECEIVE_RETRIES = 3000;
         public const int DEFAULT_RECEIVE_TIMEOUT_INNER = 1000;
         public const int DEFAULT_RECEIVE_TIMEOUT_OUTER = 15000;
         public const int DEFAULT_SEND_BUFFER_SIZE = 1024;
-        public const int DEFAULT_SEND_RETIRES = 3000;
         public const int DEFAULT_SEND_TIMEOUT = 15000;
 
-        [Reactive] public Checksum.ChecksumTypes Checksum { get; set; } = Toolbox.Checksum.ChecksumTypes.None;
-        [Reactive] public int ConnectRetries { get; set; } = DEFAULT_CONNECT_RETIRES;
-        [Reactive] public int ReceiveBufferSize { get; set; } = DEFAULT_RECEIVE_BUFFER_SIZE;
-        [Reactive] public int ReceiveRetries { get; set; } = DEFAULT_RECEIVE_RETRIES;
-        [Reactive] public int ReceiveTimeoutInner { get; set; } = DEFAULT_RECEIVE_TIMEOUT_INNER;
-        [Reactive] public int ReceiveTimeoutOuter { get; set; } = DEFAULT_RECEIVE_TIMEOUT_OUTER;
-        [Reactive] public int SendBufferSize { get; set; } = DEFAULT_SEND_BUFFER_SIZE;
-        [Reactive] public int SendRetries { get; set; } = DEFAULT_SEND_RETIRES;
-        [Reactive] public int SendTimeout { get; set; } = DEFAULT_SEND_TIMEOUT;
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public int ReceiveBufferSize { get; set; } = DEFAULT_RECEIVE_BUFFER_SIZE;
+        public int ReceiveTimeoutInner { get; set; } = DEFAULT_RECEIVE_TIMEOUT_INNER;
+        public int ReceiveTimeoutOuter { get; set; } = DEFAULT_RECEIVE_TIMEOUT_OUTER;
+        public int SendBufferSize { get; set; } = DEFAULT_SEND_BUFFER_SIZE;
+        public int SendTimeout { get; set; } = DEFAULT_SEND_TIMEOUT;
     }
 }
--- src/Toolbox/Connection/IConnectionSettings.cs
+++ src/Toolbox/Connection/IConnectionSettings.cs
@@ -2,14 +2,10 @@ namespace Toolbox.Connection
 {
     public interface IConnectionSettings
     {
-        Checksum.ChecksumTypes Checksum { get; set; }
-        int ConnectRetries { get; set; }
         int ReceiveBufferSize { get; set; }
-        int ReceiveRetries { get; set; }
         int ReceiveTimeoutInner { get; set; }
         int ReceiveTimeoutOuter { get; set; }
         int SendBufferSize { get; set; }
-        int SendRetries { get; set; }
         int SendTimeout { get; set; }
     }
 }
