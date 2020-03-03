93aa9fab1254ba20f6848ea7384e16d9832937ae
sean.alford <sean.alford@icloud.com>
2020-03-03 17:08:34 -0500

    Add ProtocolMessageData

--- src/Toolbox/Protocol/IProtocolMessage.cs
+++ src/Toolbox/Protocol/IProtocolMessage.cs
@@ -2,19 +2,19 @@
 
 namespace Toolbox.Protocol
 {
-    public interface IProtocolMessage<TMessageStatus, TMessageData> : IDisposable
-        where TMessageStatus : struct
+
+    public interface IProtocolMessage : IDisposable
     {
         byte[] Abort { get; }
         byte[] Ack { get; }
         bool Complete { get; }
-        TMessageData Data { get; }
+        IProtocolMessageData Data { get; }
         bool Decode(byte[] data);
         byte[] Encode();
         byte[] Nak { get; }
         int RxBytesToRead { get; }
         byte RxEndOfMessageToken { get; }
-        TMessageStatus Status { get; }
+        IProtocolMessageStatus Status { get; }
         int TxBytesToRead { get; }
         byte TxEndOfMessageToken { get; }
         bool ValidateTx { get; }
---
+++ src/Toolbox/Protocol/ProtocolMessage.cs
@@ -0,0 +1,35 @@
+﻿namespace Toolbox.Protocol
+{
+    public abstract class ProtocolMessage : IProtocolMessage
+    {
+        public byte[] Abort { get; protected set; }
+
+        public byte[] Ack { get; protected set; }
+
+        public bool Complete { get; protected set; }
+
+        public IProtocolMessageData Data { get; protected set; }
+
+        public abstract bool Decode(byte[] data);
+
+        public abstract void Dispose();
+
+        public abstract byte[] Encode();
+
+        public byte[] Nak { get; protected set; }
+
+        public int RxBytesToRead { get; protected set; }
+
+        public byte RxEndOfMessageToken { get; protected set; }
+
+        public IProtocolMessageStatus Status { get; protected set; }
+
+        public int TxBytesToRead { get; protected set; }
+
+        public byte TxEndOfMessageToken { get; protected set; }
+
+        public bool ValidateTx { get; protected set; }
+
+        public abstract bool ValidTx(byte[] data);
+    }
+}
