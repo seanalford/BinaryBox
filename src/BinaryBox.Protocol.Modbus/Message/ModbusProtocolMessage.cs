7990355d6ee4403d0c2f5f4ea1bac539152f6567
sean.alford <sean.alford@icloud.com>
2020-03-30 09:37:26 -0400

    Add Modbus method types.

--- src/BinaryBox.Protocol.Modbus/Message/ModbusProtocolMessage.cs
+++ src/BinaryBox.Protocol.Modbus/Message/ModbusProtocolMessage.cs
@@ -4,11 +4,18 @@ namespace BinaryBox.Protocol.Modbus
 {
     public abstract class ModbusProtocolMessage : ProtocolMessage<IModbusProtocolSettings>, IModbusProtocolMessage<IModbusProtocolSettings>
     {
+        protected byte[] ModbusId { get; private set; }
+        protected byte FunctionCode { get; private set; }
+        //protected byte[] Data { get; private set; }
+
         public ModbusProtocolMessage(ILogger logger, IModbusProtocolSettings settings) : base(logger, settings)
         {
-
-            // Why isn't the compiler compaining about not implementing the abstract messages in ProtocolMessage?
-
+            Complete = true;
+            RxBytesToRead = 0;
+            RxEndOfMessageToken = (byte)MessageTokens.ETX;
+            TxBytesToRead = 1;
+            TxEndOfMessageToken = 0;
+            ValidateTx = true;
         }
     }
 }
--- src/BinaryBox.Protocol.Modbus/Message/ModbusProtocolMessageTypes.cs
+++ src/BinaryBox.Protocol.Modbus/Message/ModbusProtocolMessageTypes.cs
@@ -1,4 +1,26 @@
 ﻿namespace BinaryBox.Protocol.Modbus
 {
-    public enum ModbusProtcolMessageTypes { } // TODO: Add modbus message types / functions here.
+    public enum ModbusProtcolMessageTypes
+    {
+        ReadCoils = 1,
+        ReadDiscreteInputs = 2,
+        ReadMultipleHoldingRegisters = 3,
+        ReadInputRegisters = 4,
+        WriteSingleCoil = 5,
+        WriteSingleHoldingRegister = 6,
+        ReadExceptionStatus = 7,
+        Diagnostic = 8,
+        GetComEventCounter = 11,
+        GetComEventLog = 12,
+        WriteMultipleCoils = 15,
+        WriteMultipleHoldingRegisters = 16,
+        ReportSlaveID = 17,
+        ReadFileRecord = 20,
+        WriteFileRecord = 21,
+        MaskWriteRegister = 22,
+        ReadWriteMultipleRegisters = 23,
+        ReadFIFOQueue = 24,
+        ReadDeviceIdentification = 43,
+        //EncapsulatedInterfaceTransport = 43,
+    }
 }
