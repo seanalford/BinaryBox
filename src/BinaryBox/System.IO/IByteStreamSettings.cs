8271482287ac4729515bc3354aa22c04f2715fb5
sean.alford <sean.alford@icloud.com>
2020-04-21 14:31:28 -0400

    Added - ByteStreamManager - ByteStreamManagerResponse - ByteStreamManagerResponseStatus - ByteStreamManagerResponseStatusCode - ByteStreamState - IByteStream - IByteStreamManager - IByteStreamSettings

---
+++ src/BinaryBox/System.IO/IByteStreamSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BinaryBox.Core.System.IO
+{
+    public interface IByteStreamSettings : IDisposable
+    {
+        int OpenCloseTimeout { get; set; }
+        int BufferSize { get; set; }
+        int ReadPrimaryTimeout { get; set; }
+        int ReadSecondaryTimeout { get; set; }
+        int WriteTimeout { get; set; }
+    }
+
+}
