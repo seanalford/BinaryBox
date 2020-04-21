3778d7bb3b1f09acbdba59c4418d15fad1be58f8
sean.alford <sean.alford@icloud.com>
2020-04-21 14:27:43 -0400

    Added IResponse, and IResponseStatus

---
+++ src/BinaryBox/Response/IResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BinaryBox.Core.Response
+{
+    public interface IResponse<TStatus, TStatusCode, TData>
+        where TStatus : IResponseStatus<TStatusCode>
+        where TStatusCode : struct
+    {
+        TStatus Status { get; }
+        TData Data { get; }
+    }
+
+}
