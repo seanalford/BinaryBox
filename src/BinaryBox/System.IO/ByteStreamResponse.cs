85109c9fda6a4471f4db59d7250628438081082b
sean.alford <sean.alford@icloud.com>
2020-04-23 19:23:31 -0400

    Renamed ByteStreamManagerResponse to ByteStreamResponse

---
+++ src/BinaryBox/System.IO/ByteStreamResponse.cs
@@ -0,0 +1,12 @@
+﻿using BinaryBox.Core.Response;
+
+namespace BinaryBox.Core.System.IO
+{
+    public class ByteStreamResponse<TData> : Response<ByteStreamResponseStatus, ByteStreamResponseStatusCode, TData>, IResponse<ByteStreamResponseStatus, ByteStreamResponseStatusCode, TData>
+    {
+        public ByteStreamResponse(ByteStreamResponseStatusCode code, TData data = default) : base(code, data)
+        {
+            Status = new ByteStreamResponseStatus(code);
+        }
+    }
+}
