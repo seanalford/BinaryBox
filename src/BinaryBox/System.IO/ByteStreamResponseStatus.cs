1cb4ac39c64426b49ed415574c2112f5057d0a1f
sean.alford <sean.alford@icloud.com>
2020-04-23 19:24:16 -0400

    Renamed ByteStreamManagerResponseStatus to ByteStreamResponseStatus

--- src/BinaryBox/System.IO/ByteStreamResponseStatus.cs
+++ src/BinaryBox/System.IO/ByteStreamResponseStatus.cs
@@ -2,9 +2,9 @@
 
 namespace BinaryBox.Core.System.IO
 {
-    public class ByteStreamManagerResponseStatus : ResponseStatus<ByteStreamManagerResponseStatusCode>, IResponseStatus<ByteStreamManagerResponseStatusCode>
+    public class ByteStreamResponseStatus : ResponseStatus<ByteStreamResponseStatusCode>, IResponseStatus<ByteStreamResponseStatusCode>
     {
-        public ByteStreamManagerResponseStatus(ByteStreamManagerResponseStatusCode code) : base(code)
+        public ByteStreamResponseStatus(ByteStreamResponseStatusCode code) : base(code)
         {
         }
 
@@ -13,39 +13,39 @@ namespace BinaryBox.Core.System.IO
             //TODO: Move string literals to english resource file.
             switch (Code)
             {
-                case ByteStreamManagerResponseStatusCode.OK:
+                case ByteStreamResponseStatusCode.OK:
                     Success = true;
                     Description = "Operation Succesfully";
                     break;
-                case ByteStreamManagerResponseStatusCode.Cancelled:
+                case ByteStreamResponseStatusCode.Cancelled:
                     Success = false;
                     Description = "Operation Failed: Cancelled";
                     break;
-                case ByteStreamManagerResponseStatusCode.OpenCloseTimeout:
+                case ByteStreamResponseStatusCode.OpenCloseTimeout:
                     Success = false;
                     Description = "Operation Failed: Timed Out";
                     break;
-                case ByteStreamManagerResponseStatusCode.AlreadyClosed:
+                case ByteStreamResponseStatusCode.AlreadyClosed:
                     Success = false;
                     Description = "Operation Failed: Already Closed";
                     break;
-                case ByteStreamManagerResponseStatusCode.AlreadyOpen:
+                case ByteStreamResponseStatusCode.AlreadyOpen:
                     Success = false;
                     Description = "Operation Failed: Already Open";
                     break;
-                case ByteStreamManagerResponseStatusCode.NotOpen:
+                case ByteStreamResponseStatusCode.NotOpen:
                     Success = false;
                     Description = "Operation Failed: Not Open";
                     break;
-                case ByteStreamManagerResponseStatusCode.WriteTimeout:
+                case ByteStreamResponseStatusCode.WriteTimeout:
                     Success = false;
                     Description = "Operation Failed: Write Timeout";
                     break;
-                case ByteStreamManagerResponseStatusCode.PrimaryReadTimeout:
+                case ByteStreamResponseStatusCode.PrimaryReadTimeout:
                     Success = false;
                     Description = "Operation Failed: Primary Read Timeout";
                     break;
-                case ByteStreamManagerResponseStatusCode.SecondaryReadTimeout:
+                case ByteStreamResponseStatusCode.SecondaryReadTimeout:
                     Success = false;
                     Description = "Operation Failed: Secondary Read Timeout";
                     break;
