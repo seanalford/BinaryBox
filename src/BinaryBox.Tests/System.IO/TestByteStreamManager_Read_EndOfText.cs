128acfe4578ebeb5e9f5262bde4e86cfa05d8526
taylorshank <53275873+taylorshank@users.noreply.github.com>
2020-05-04 15:43:49 -0400

    Feature/byte stream manager tests (#49)
    * Added more test conditions to TestSuccess
    
    * Add TestSuccess
    - Add TestCancel
    - Add TestNotOpen
    - Add TestPrimaryTimeout
    - Add TestSecondaryTimeout
    - Add TestUnhandledException
    
    * Add TestSuccess
    
    * Add TestCancel
    
    * Add TestNotOpen
    
    * Add TestUnhandledException
    
    Co-authored-by: Taylor Shank <taylor.shank@eagleresearchcorp.com>

--- src/BinaryBox.Tests/System.IO/TestByteStreamManager_Read_EndOfText.cs
+++ src/BinaryBox.Tests/System.IO/TestByteStreamManager_Read_EndOfText.cs
@@ -17,56 +17,118 @@ namespace BinaryBox.Core.System.IO.Test
         // - SecondaryTimeout        
         // - Unhandled Exception                
 
-        [Fact]
-        public async Task TestSuccess()
+
+        [Theory]
+        [InlineData(1, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new byte[] { 1 })]
+        [InlineData(2, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new byte[] { 1, 2 })]
+        [InlineData(3, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new byte[] { 1, 2, 3 })]
+        [InlineData(4, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new byte[] { 1, 2, 3, 4 })]
+        [InlineData(5, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new byte[] { 1, 2, 3, 4, 5 })]
+        public async Task TestSuccess(byte endOfText, byte[] data, byte[] expected)
         {
-            // Arrange                
+            // Arrange    
+            int index = 0;
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.DataAvailableAsync().Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.OK, true));
+            byteStream.ReadAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<int>(ByteStreamResponseStatusCode.OK, expected.Length));
+            byteStream.WhenForAnyArgs(x => x.ReadAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()))
+                .Do(x =>
+                {
+                    x.Arg<byte[]>()[0] = data[index++];
+                });
+
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
 
             // Act
+            var result = await byteStreamManager.ReadAsync(endOfText);
 
             // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.OK);
+            result.Success.Should().BeTrue();
+            result.Data.Should().BeEquivalentTo(expected);
+
         }
 
         [Fact]
         public async Task TestCancel()
         {
-            // Arrange                
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.OpenAsync().Returns(new ByteStreamResponse<ByteStreamState>(ByteStreamResponseStatusCode.OK, ByteStreamState.Open));
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.DataAvailableAsync().Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.OK, true));
+            byteStream.ReadAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<int>(ByteStreamResponseStatusCode.Cancelled, 0));
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
 
             // Act
+            // NOTE: The cancellationTokenSource.Token here does not actually cause the cancellation as you might expect.
+            //       It has been added to the test for completeness.  The byteStreamMananager simply passes the token along            
+            //       to the byteStream.  In this case the byteStream substitution is faking the cancallation for us.
+            var result = await byteStreamManager.ReadAsync((byte)2, 0, cancellationTokenSource.Token);
 
             // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.Cancelled);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeEquivalentTo(default);
         }
 
 
         [Fact]
         public async Task TestNotOpen()
         {
-            // Arrange                             
+            // Arrange                        
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
 
             // Act
+            var result = await byteStreamManager.ReadAsync((byte)10);
 
-            // Assert            
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.NotOpen);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeEquivalentTo(default);
 
         }
 
         [Fact]
         public async Task TestPrimaryTimeout()
         {
-            // Arrange                             
+            // Arrange                        
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.OpenAsync().Returns(new ByteStreamResponse<ByteStreamState>(ByteStreamResponseStatusCode.OK, ByteStreamState.Open));
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.DataAvailableAsync().Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.OK, false));
+            byteStream.ReadAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<int>(ByteStreamResponseStatusCode.OK, 0));
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
 
-            // Act            
+            // Act          
+            var result = await byteStreamManager.ReadAsync((byte)10);
 
             // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.PrimaryReadTimeout);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeEquivalentTo(default);
         }
 
         [Fact]
         public async Task TestSecondaryTimeout()
         {
-            // Arrange                             
+            // Arrange                        
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.OpenAsync().Returns(new ByteStreamResponse<ByteStreamState>(ByteStreamResponseStatusCode.OK, ByteStreamState.Open));
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.DataAvailableAsync().Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.OK, true));
+            byteStream.ReadAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<int>(ByteStreamResponseStatusCode.OK, 0));
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
 
-            // Act            
+            // Act          
+            var result = await byteStreamManager.ReadAsync((byte)10);
 
-            // Assert            
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.SecondaryReadTimeout);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeEquivalentTo(default);
         }
 
         [Fact]
@@ -82,7 +144,7 @@ namespace BinaryBox.Core.System.IO.Test
             IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
 
             // Act
-            Func<Task> func = async () => { await byteStreamManager.ReadAsync(10); };
+            Func<Task> func = async () => { await byteStreamManager.ReadAsync((byte)10); };
 
             // Assert
             await func.Should().ThrowAsync<Exception>();
---
+++ src/BinaryBox.Tests/System.IO/TestByteStreamManager_Write.cs
@@ -0,0 +1,97 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BinaryBox.Core.System.IO.Test
+{
+    public class TestByteStreamManager_Write
+    {
+        //Success​
+        //Failure
+        //Cancel​
+        //Not Open        ​
+        //Timeout​
+        //Unhandled Exception
+
+        [Theory]
+        [InlineData( new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 } )]
+        public async Task TestSuccess(byte[] data)
+        {
+            // Arrange    
+            int index = 0;
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.WriteAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.OK, true));
+
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
+
+            // Act
+            var result = await byteStreamManager.WriteAsync(data, default);
+
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.OK);
+            result.Success.Should().BeTrue();
+            result.Data.Should().BeTrue();
+
+        }
+
+        [Fact]
+        public async Task TestCancel()
+        {
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.WriteAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.Cancelled, false));
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+
+            // Act
+            // NOTE: The cancellationTokenSource.Token here does not actually cause the cancellation as you might expect.
+            //       It has been added to the test for completeness.  The byteStreamMananager simply passes the token along            
+            //       to the byteStream.  In this case the byteStream substitution is faking the cancallation for us.
+            var result = await byteStreamManager.WriteAsync(new byte[] { 0, 1, 2 }, cancellationTokenSource.Token);
+
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.Cancelled);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task TestNotOpen()
+        {
+            // Arrange                        
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
+
+            // Act
+            var result = await byteStreamManager.WriteAsync(new byte[] { 0, 1, 2 }, default);
+
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.NotOpen);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeFalse();
+
+        }
+
+        [Fact]
+        public async Task TestUnhandledException()
+        {
+            // Arrange                             
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.When(x => x.WriteAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>())).Do(x => { throw new Exception(); });
+
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
+
+            // Act
+            Func<Task> func = async () => { await byteStreamManager.WriteAsync(new byte[] { 0, 1, 2 }, default); };
+
+            // Assert
+            await func.Should().ThrowAsync<Exception>();
+
+        }
+    }
+}
