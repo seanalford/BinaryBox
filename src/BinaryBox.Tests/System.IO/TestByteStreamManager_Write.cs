54027b9a2e47827ff8a7d0cf86b7943bb564542c
Taylor Shank <taylor.shank@eagleresearchcorp.com>
2020-05-04 14:04:26 -0400

    Add TestCancel

--- src/BinaryBox.Tests/System.IO/TestByteStreamManager_Write.cs
+++ src/BinaryBox.Tests/System.IO/TestByteStreamManager_Write.cs
@@ -37,5 +37,26 @@ namespace BinaryBox.Core.System.IO.Test
             result.Data.Should().BeTrue();
 
         }
+
+        [Fact]
+        public async Task TestCancel()
+        {
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.WriteAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.Cancelled, false));
+            IByteStreamManager byteStreamManager = new ByteStreamManager(byteStream, new ByteStreamSettings());
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+
+            // Act
+            // NOTE: The cancellationTokenSource.Token here does not actually cause the cancellation as you might expect.
+            //       It has been added to the test for completeness.  The byteStreamMananager simply passes the token along            
+            //       to the byteStream.  In this case the byteStream substitution is faking the cancallation for us.
+            var result = await byteStreamManager.WriteAsync(new byte[] { 0, 1, 2 }, cancellationTokenSource.Token);
+
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.Cancelled);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeFalse();
+        }
     }
 }
