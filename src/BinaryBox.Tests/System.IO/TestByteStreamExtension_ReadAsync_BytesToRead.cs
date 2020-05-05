f1561154f27a094f153dcb22bd726b28ba205d62
Taylor Shank <taylor.shank@eagleresearchcorp.com>
2020-05-05 16:28:11 -0400

    Add TestCancel

--- src/BinaryBox.Tests/System.IO/TestByteStreamExtension_ReadAsync_BytesToRead.cs
+++ src/BinaryBox.Tests/System.IO/TestByteStreamExtension_ReadAsync_BytesToRead.cs
@@ -42,7 +42,29 @@ namespace BinaryBox.Tests.System.IO
             result.Status.Should().Be(ByteStreamResponseStatusCode.OK);
             result.Success.Should().BeTrue();
             result.Data.Should().BeEquivalentTo(expected);
+        }
 
+        [Fact]
+        public async Task TestCancel()
+        {
+            // Arrange                        
+            IByteStream byteStream = Substitute.For<IByteStream>();
+            byteStream.OpenAsync().Returns(new ByteStreamResponse<ByteStreamState>(ByteStreamResponseStatusCode.OK, ByteStreamState.Open));
+            byteStream.State.Returns(ByteStreamState.Open);
+            byteStream.DataAvailableAsync().Returns(new ByteStreamResponse<bool>(ByteStreamResponseStatusCode.OK, true));
+            byteStream.ReadAsync(Arg.Any<byte[]>(), Arg.Any<int>(), Arg.Any<int>(), Arg.Any<CancellationToken>()).Returns(new ByteStreamResponse<int>(ByteStreamResponseStatusCode.Cancelled, 0));
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromMilliseconds(100));
+
+            // Act
+            // NOTE: The cancellationTokenSource.Token here does not actually cause the cancellation as you might expect.
+            //       It has been added to the test for completeness.  The byteStreamMananager simply passes the token along            
+            //       to the byteStream.  In this case the byteStream substitution is faking the cancallation for us.
+            var result = await byteStream.ReadAsync(10, cancellationTokenSource.Token);
+
+            // Assert
+            result.Status.Should().Be(ByteStreamResponseStatusCode.Cancelled);
+            result.Success.Should().BeFalse();
+            result.Data.Should().BeEquivalentTo(default);
         }
     }
 }
